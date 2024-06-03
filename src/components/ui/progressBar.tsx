615dc6b290c8343684e50fd09824ae66965501c2
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2024-06-03 07:46:31 +0530

    refactored some more code

--- src/components/ui/progressBar.tsx
+++ src/components/ui/progressBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { percentage } from "../../utils/helper";
 
 interface ProgressBarProps {
   masteredWords: number;
@@ -9,20 +10,18 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
   masteredWords,
   totalWords,
 }) => {
-  let percentage = Math.floor((masteredWords / totalWords) * 100);
-
   return (
     <div className="mb-6">
       <div className="flex items-center mb-1">
         <p className="text-sm font-semibold text-gray-700">Progress:</p>
         <p className="text-sm font-semibold text-gray-700 ml-auto">
-          {percentage}%
+          {percentage(masteredWords, totalWords)}%
         </p>
       </div>
       <div className="w-full bg-gray-200 h-3 rounded-full shadow-inner overflow-hidden">
         <div
           className="bg-gradient-to-r from-green-400 to-green-600 h-full rounded-full shadow-md"
-          style={{ width: `${percentage}%` }}
+          style={{ width: `${percentage(masteredWords, totalWords)}%` }}
         ></div>
       </div>
     </div>
