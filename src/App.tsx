0c8d2eb8d0b15732da405891189262b305f66f6c
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2024-06-03 22:58:06 +0530

    moved flashcards to a different route

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,16 @@
-import { useState, useEffect } from "react";
-import Card from "./components/card";
-import Sidebar from "./components/sideBar";
-import barron from "./assets/barrons.json";
-import dummy from "./assets/dummy.json";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
+import Flashcard from "./flashcard";
 
 const App = () => {
-  const [selected, setSelected] = useState("dummy");
-  const [words, setWords] = useState(dummy);
-  const [wordStatus, setWordStatus] = useState<{
-    [key: number]: "mastered" | "learning" | "untouched";
-  }>(() => {
-    const initialStatus: {
-      [key: number]: "mastered" | "learning" | "untouched";
-    } = {};
-    dummy.forEach((_, index) => {
-      initialStatus[index] = "untouched";
-    });
-    return initialStatus;
-  });
-
-  useEffect(() => {
-    if (selected === "barrons 333") {
-      setWords(barron);
-    } else if (selected === "dummy") {
-      setWords(dummy);
-    }
-
-    // Reset wordStatus when words change
-    const initialStatus: {
-      [key: number]: "mastered" | "learning" | "untouched";
-    } = {};
-    const selectedWords = selected === "barrons 333" ? barron : dummy;
-    selectedWords.forEach((_, index) => {
-      initialStatus[index] = "untouched";
-    });
-    setWordStatus(initialStatus);
-  }, [selected]);
-
   return (
-    <div className="flex gap-x-4">
-      <Sidebar
-        selected={selected}
-        setSelected={setSelected}
-        wordStatus={wordStatus}
-        totalWords={words.length}
-      />
-      <div className="mr-4 flex-1 h-screen p-4 pl-0">
-        <Card
-          words={words}
-          wordStatus={wordStatus}
-          setWordStatus={setWordStatus}
-        />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/flashcard" element={<Flashcard />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
