76d4ef872f1b95fec6cd0bb3391793ab660ed91d
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2024-06-06 19:55:55 +0530

    fixed margin

--- src/components/wordStatus.tsx
+++ src/components/wordStatus.tsx
@@ -9,7 +9,7 @@ interface WordStatusProps {
 
 const WordStatus: React.FC<WordStatusProps> = ({ wordStatus, totalWords }) => {
   return (
-    <div className="space-y-6 flex-1 overflow-y-auto overscroll-contain">
+    <div className="p-6 space-y-6 flex-1 overflow-y-auto overscroll-contain">
       <div className="clear-both flex justify-center">
         <div className="inline-flex rounded-md shadow-sm">
           <div className="py-3 px-4 inline-flex justify-center items-center gap-2 -ml-px first:rounded-l-lg first:ml-0 last:rounded-r-lg border font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all text-sm ">
