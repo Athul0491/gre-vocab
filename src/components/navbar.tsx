897b99d367c9f0e1a71a48d27ccd2f4a8648758f
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2024-06-07 12:24:46 +0530

    updated navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,20 +12,23 @@ const Navbar = () => {
       <nav className="nav font-semibold text-lg">
         <ul className="flex items-center">
           <li className="p-3 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
-            <a href="">Home</a>
+            <a href="">Flashcard</a>
           </li>
           <li className="p-3 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
-            <a href="">About us</a>
+            <a href="">Definition match</a>
           </li>
           <li className="p-3 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
-            <a href="">Library</a>
+            <a href="">Synonyms Practice</a>
           </li>
           <li className="p-3 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
-            <a href="">Contact</a>
+            <a href="">Sentence Equivalence</a>
           </li>
         </ul>
       </nav>
-      <div className="w-3/12 flex justify-end">
+      <div className="w-3/12 flex justify-end items-center space-x-4">
+        {/* Welcome Message */}
+        <span className="text-gray-700 font-medium">Welcome back, Athul</span>
+        {/* User Dropdown */}
         <UserDropdown />
       </div>
     </header>
